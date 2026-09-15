55e929b42807f91f3e70c177ef08938f3772d293
Matheus <MATHEUSMORANTE002@GMAIL.COM>
2026-09-15 09:47:04 -0300

    m

--- erp/src/pages/App/SalesOrder/useSalesOrderForm.ts
+++ erp/src/pages/App/SalesOrder/useSalesOrderForm.ts
@@ -1,3 +1,2 @@
 // Re-export para retrocompatibilidade arquitetural (Skill: organizacao-arquivos-diretorios)
 export * from './hooks/useSalesOrderForm';
-export { default } from './hooks/useSalesOrderForm';
